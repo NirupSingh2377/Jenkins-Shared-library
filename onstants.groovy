635d5f39bfdf2f0163ff1cdc8aa4d5d17218136a
NirupSingh2377 <nirupsinghsagar@gmail.com>
2025-02-04 22:26:59 +0530

    Create onstants.groovy

---
+++ onstants.groovy
@@ -0,0 +1,10 @@
+def call() {
+    return [
+        AWS_ACCOUNT_ID: '390403869878',
+        AWS_DEFAULT_REGION: 'us-east-1',
+        IMAGE_REPO_NAME: 'sample-webapp',
+        GIT_REPO_URL: 'https://github.com/NirupSingh2377/sample-webapp.git',
+        GIT_CREDENTIALS_ID: 'NirupSingh2377',
+        ECR_CREDENTIALS_ID: 'Nirup@12345'
+    ]
+}
